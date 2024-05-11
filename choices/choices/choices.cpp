e26e9487e3da2346946ef7ed13cbd957f468a517
Ralph Napier <RalphNapierii@gmail.com>
2024-05-11 13:56:58 -0400

    First commit

---
+++ choices/choices/choices.cpp
@@ -0,0 +1,164 @@
+// choices.cpp : This file contains the 'main' function. Program execution begins and ends there.
+#include <chrono>;
+#include <thread>;
+#include <iostream>;
+using namespace std;
+
+//string chorus = ""
+
+
+int main()
+{
+int resp = 1;
+int score =  resp * 1;
+
+	cout << "Ever told on a nigga?\n";
+	cin >> resp;
+	this_thread::sleep_for(.6s);
+		cout << "Ever squeezed a trigger?\n";
+		cin >> resp;
+		this_thread::sleep_for(.6s);
+		cout << "Ever set a nigga up?\n";
+		cin >> resp;
+		this_thread::sleep_for(.6s);
+		cout << "Ever helped a brother out when he was down on his luck?\n";
+		cin >> resp;
+		this_thread::sleep_for(.6s);
+			cout << "You a sap?\n";
+			cin >> resp;
+			this_thread::sleep_for(.6s);
+				cout << "You a boss player, you a mack?\n";
+			cin >> resp;
+			this_thread::sleep_for(.6s);
+				cout << "Let me hold a couple dollars?\n";
+				cin >> resp;
+				this_thread::sleep_for(.6s);
+				cout << "Y'all still be poppin' y'all collars?\n";
+					cin >> resp;
+					this_thread::sleep_for(.6s);
+					cout << "Stock rims on a scraper?\n";
+					cin >> resp;
+					this_thread::sleep_for(.6s);
+						cout << "Paint wetter than a lake?\n";
+						cin >> resp;
+						this_thread::sleep_for(1s);
+/*	cout << ""Poodle in my blood(nope)
+cin >> ""
+		cout << ""Bitch, I'm a thug (yup)
+cin >> ""
+		cout << ""You a loser ? (Nope), Winner ? (Yup)
+cin >> ""
+		cout << ""Starving ? (nope), Dinner ? (Yup)
+cin >> ""
+		cout << ""You still sell dope ? (Nope)
+cin >> ""
+		cout << ""Now you cleaner than a bar of Dove soap ? (Yup)
+cin >> ""
+		cout << ""Got a little gouda(nope) ?
+cin >> ""
+		cout << ""Got a thumper, got a Ruger ? (Yup)
+cin >> ""
+		cout << ""You in love wit' the ho? (Nope)
+cin >> ""
+		cout << ""She bringin' you the dough? (Yup)
+cin >> ""
+		cout << ""You gon' cry if she leave? (Nope)
+cin >> ""
+		cout << ""You gon' fly overseas? (Yup)
+cin >> ""
+
+
+		cout << ""Everybody get choices
+		cout << ""I choose to get money, I'm stuck to this bread
+		cout << ""Everybody got choices
+		cout << ""These bitches is choosin', I'm all in they head
+		cout << ""Everybody got choices
+		cout << ""Keep it 1 thou, I'da liked it, I chose
+		cout << ""Everybody got choices
+		cout << ""These niggas be hatin', I already know
+		cout << ""But I never go broke(no, no, no)
+		cout << ""I'mma stay gettin' money(yeah, yeah, yeah)
+		cout << ""And I ain't gotta sell my soul (no, no, no)
+		cout << ""I'mma stay gettin' money(yeah, yeah, yeah)
+		cout << ""If you broke, you ain't like me (no, no, no)
+		cout << ""I'mma stay gettin' money(yeah, yeah, yeah)
+		I give a fuck if you don't like me (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+
+
+		Ugh, lazy ? (Nope)
+		Got dick that'll drive a ho crazy? (Yup)
+		Hater ? (nope), Wanna see a player get paper ? (Yup)
+		Traitor ? (nope), Loyal to my soil, not a faker ? (Yup)
+		Sleep ? (nope), Bust moves, hella active in the streets ? (Yup)
+		Scared of the dark ? (Nope)
+		Have money, have heart ? (Yup)
+		Narc ? (nope), Shark ? (Yup)
+		Gossip like a broad ? (Nope)
+		Check a bitch like a smog ? (Yup)
+		Slippin'? (nope), Trippin' ? (Yup)
+		Sober as a gopher ? (Nope)
+		Higher than a rollercoaster ? (Yup)
+		Star Wars ? (Nope), Yoda ? (Yup)
+		Never leave the house without my strap(nope)
+		Shoot a muthafucka in his nap(yup)
+		Never been a sucka or a pussy(nope)
+		Blowin' cookie, never coughin' like a rookie(yup)
+
+
+		Everybody get choices
+		I choose to get money, I'm stuck to this bread
+		Everybody got choices
+		These bitches is choosin', I'm all in they head
+		Everybody got choices
+		Keep it 1 thou, I'da liked it, I chose
+		Everybody got choices
+		These niggas be hatin', I already know
+		But I never go broke(no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		And I ain't gotta sell my soul (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		If you broke, you ain't like me (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		I give a fuck if you don't like me (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+
+
+		Was it love at first sight ? (Nope)
+		Did she ride you like a bike ? (Yup)
+		Was it ripe ? (Nope), Was her pussy tight ? (Yup)
+		Got some property ? (Nope), Drive a Maserati ? (Yup)
+		You softer than a sock ? (Nope)
+		You solid as a rock ? (Yup)
+		Slow ? (nope), Trained to go ? (Yup)
+		Your team weak ? (Nope)
+		You respected in the stree - neets ? (Yup)
+		Shallow ? (nope), Di - neep ? (Yup)
+		Broke ? (nope), Chi - neat ? (Yup)
+		Not a BB or a pellet gun(nope)
+		But a long barrel base drum(yup)
+		If I get into it will run(nope)
+		I'mma give a nigga fair one (yup)
+		Still live in the trap ? (Nope)You ever go back ? (Yup)
+		Wear a wire and a camera ? (Nope)Ears to the scanner ? (Yup)
+
+
+		Everybody get choices
+		I choose to get money, I'm stuck to this bread
+		Everybody got choices
+		These bitches is choosin', I'm all in they head
+		Everybody got choices
+		Keep it 1 thou, I'da liked it, I chose
+		Everybody got choices
+		These niggas be hatin', I already know
+		But I never go broke(no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		And I ain't gotta sell my soul (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		If you broke, you ain't like me (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah)
+		I give a fuck if you don't like me (no, no, no)
+		I'mma stay gettin' money(yeah, yeah, yeah) */
+this_thread::sleep_for(3s);
+cout << resp;
+}
