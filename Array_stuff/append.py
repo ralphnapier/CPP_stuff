6db62dcf2032edcd20dddeb85d31509e05b68e96
Ralph Napier <167547084+ralphnapier@users.noreply.github.com>
2024-11-19 15:27:38 -0500

    Delete Array_stuff directory

--- Array_stuff/append.py
+++
@@ -1,6 +0,0 @@
-numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5]
-new_numbers = []
-for number in numbers:
-    if number > 5:
-        new_numbers.append(number)
-print(new_numbers)
--- Array_stuff/find_in_array.py
+++
@@ -1,9 +0,0 @@
-things = ['This', 'is', 'a', 'list']
-thing_to_find = 'is'
-found = False
-
-for string in things:
-    if string == thing_to_find:
-        found = True
-        break
-print(found)
